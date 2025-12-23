006b48d6e8f5797a9251185cb205fbcf317f72d2
Mohit Thapa <thapamohit2058@gmail.com>
2025-12-23 21:09:17 +0545

    make reusable heading component

---
+++ components/heading.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export const Heading = ({
+  className,
+  children,
+  as = "h2",
+}: {
+  className?: string;
+  children: React.ReactNode;
+  as?: "h1" | "h2";
+}) => {
+  const Tag = as;
+  return (
+    <Tag
+      className={cn(
+        "text-2xl md:text-4xl lg:text-6xl tracking-tight font-semibold",
+        className
+      )}
+    >
+      {children}
+    </Tag>
+  );
+};
