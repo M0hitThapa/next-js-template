222f4927b15d58815373501566b7db85c2985b4f
Mohit Thapa <thapamohit2058@gmail.com>
2025-12-23 21:10:31 +0545

    make reusable subheading and logo component

---
+++ components/subheading.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export const SubHeading = ({
+  className,
+  children,
+  as = "p",
+}: {
+  className?: string;
+  children: React.ReactNode;
+  as?: "h1" | "h2" | "p";
+}) => {
+  const Tag = as;
+  return (
+    <Tag
+      className={cn(
+        "text-base md:text-lg text-neutral-400 dark:text-neutral-600 max-w-xl ",
+        className
+      )}
+    >
+      {children}
+    </Tag>
+  );
+};
