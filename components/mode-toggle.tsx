d958fc010c58b01295b0769a70e2d00156416df8
Mohit Thapa <thapamohit2058@gmail.com>
2025-12-23 21:10:00 +0545

    use next theme for dark and light mode support

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export const ModeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  const [systemTheme, setSystemTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme:dark)");
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setSystemTheme(e.matches ? "dark" : "light");
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  const SWITCH = () => {
+    switch (theme) {
+      case "light": {
+        setTheme("dark");
+        return;
+      }
+      case "dark": {
+        setTheme("light");
+        return;
+      }
+      case "system": {
+        setTheme(systemTheme === "dark" ? "light" : "dark");
+        return;
+      }
+      default: {
+        return;
+      }
+    }
+  };
+  return (
+    <button
+      onClick={SWITCH}
+      className="size-10 flex items-center justify-center relative "
+    >
+      <SunIcon
+        size={10}
+        className="rotate-0 scale-100 transition-all duration-200 dark:rotate-90 dark:scale-0 text-neutral-900 dark:text-neutral-200 absolute inset-0"
+      />
+      <MoonIcon
+        size={10}
+        className="rotate-90 scale-0 transition-all duration-200 dark:rotate-0 dark:scale-100 text-neutral-900 dark:text-neutral-200 absolute inset-0"
+      />
+    </button>
+  );
+};
